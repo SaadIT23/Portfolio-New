6b6ce50fdbb53327234886f76a1b2824608d866a
Saad Imran <boyit875@gmail.com>
2025-02-04 22:29:08 +0500

    update server

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://portfolio-new-frontend.vercel.app"],
+        methods: ["POST", "GET"]
+    }
+));
 app.use(bodyParser.json());
 
 // Connect to MongoDB
