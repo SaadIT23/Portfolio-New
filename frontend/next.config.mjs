604491474c8d1afc949d9ea7a26eacfc6b262e02
Saad Imran <boyit875@gmail.com>
2025-02-04 22:42:34 +0500

    update next

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // Allow all origins
+                    { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
